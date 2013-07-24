3a20b750021f7c7741fe271320753f0537794349
Jose David Pacheco Valedo <josepdvahn@me.com>
2013-07-24 13:21:02 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KlgApp::Application.config.secret_token = 'b6b85068a48c2d1589d6491ed85625765a86f9ed10ab9ccb5469eb51fbe0d974011e7456367bd1e33ba4042b22c36841816ffc6e29f55542cfd8f043ba05e217'
